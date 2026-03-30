8bea6e80e0e11509eec17933beb022b2b777e828
Michael Schout <schoutm@gmail.com>
2026-03-30 12:15:17 -0500

    Validate version before publishing

--- conventions/src/main/kotlin/mschout.maven-publish-conventions.gradle.kts
+++ conventions/src/main/kotlin/mschout.maven-publish-conventions.gradle.kts
@@ -2,3 +2,16 @@ plugins {
   id("com.vanniktech.maven.publish")
   id("org.jetbrains.dokka")
 }
+
+val validateVersion by tasks.registering {
+  doLast {
+    val version = project.version.toString()
+    if (version.endsWith("dirty")) {
+      throw GradleException("Cannot publish a dirty version: $version")
+    }
+  }
+}
+
+tasks.withType<AbstractPublishToMaven>().configureEach {
+  dependsOn(validateVersion)
+}
